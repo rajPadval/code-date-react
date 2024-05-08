091c8f12dd1892063790b9f664ac8cc7cd34c93f
rajPadval <97424854+rajPadval@users.noreply.github.com>
2024-05-08 21:54:22 +0530

    Added changes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const port = 5000;
+const cors = require("cors");
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
@@ -10,6 +11,12 @@ const io = new Server(server, {
   },
 });
 require("dotenv").config();
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("<center> <h1>Hello world</h1> </center>");
