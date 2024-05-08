4e7aaec9f8c9f204c8770a5e323a40c6d450061e
rajPadval <97424854+rajPadval@users.noreply.github.com>
2024-05-08 20:54:50 +0530

    Added the hello route

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 // AUTHENTICATION ROUTES
-
+router.get("/hello", (req, res) =>
+  res.send("<center> <h1>Hello world</h1> </center>")
+);
 
 module.exports = router;
